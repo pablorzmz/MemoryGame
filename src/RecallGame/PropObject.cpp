367996ccd3fdc729d98d72bacfad1ef3f5678b14
PabloPC <you@example.com>
2017-07-09 18:05:34 -0600

    Se agrega clase PropObject para manejar objetos que no se animan

---
+++ src/RecallGame/PropObject.cpp
@@ -0,0 +1,6 @@
+#include "PropObject.h"
+
+PropObject::PropObject()
+{
+
+}
---
+++ src/RecallGame/PropObject.h
@@ -0,0 +1,11 @@
+#ifndef PROPOBJECT_H
+#define PROPOBJECT_H
+
+
+class PropObject
+{
+public:
+    PropObject();
+};
+
+#endif // PROPOBJECT_H
